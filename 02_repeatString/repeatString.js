5de971724720c73f1bb54184257e9f4895287d12
HasanMaaraj <123746697+HasanMaaraj@users.noreply.github.com>
2023-04-12 05:04:54 +0300

    Solve excersices 02_repeatString and 03_reverseString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(s, num) {
+    if (num < 0 ){
+        return "ERROR"
+    }
+    let ans="";
+    let i=0;
+    while (i<num) {
+        ans += s
+        i++
+    }
+    return ans
 };
 
 // Do not edit below this line
