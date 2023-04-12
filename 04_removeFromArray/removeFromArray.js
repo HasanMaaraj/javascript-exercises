76116aca1fb18af54800cbeccfd3238f171aa22e
HasanMaaraj <123746697+HasanMaaraj@users.noreply.github.com>
2023-04-12 05:50:37 +0300

    Solve excersice 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...rm) {
+ for (let i = 0; i < arr.length;){
+    if (rm.includes(arr[i])){
+        arr.splice(i,1);
+    }
+    else{
+        i++;
+    }
+ }
+ return arr;
 };
 
 // Do not edit below this line
