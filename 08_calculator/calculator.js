cb57d0df59798d224319e32129bf638210e52025
HasanMaaraj <123746697+HasanMaaraj@users.noreply.github.com>
2023-04-20 07:35:18 +0300

    Solve excercise 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	let ans = 0;
+  for (let number of numbers) {
+    ans += number;
+  }
+  return ans;
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  let ans = 1;
+  for (let number of numbers) {
+    ans *= number;
+  }
+  return ans;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  ans = 1;
+  for (let i = n; i > 0; i--) {
+    ans *= i;
+  }
+  return ans;
 };
 
 // Do not edit below this line
