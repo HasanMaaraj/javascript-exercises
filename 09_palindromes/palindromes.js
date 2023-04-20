9107a7bd49cfeef1e6e249afb73a35ecfa5fde41
HasanMaaraj <123746697+HasanMaaraj@users.noreply.github.com>
2023-04-20 07:57:10 +0300

    Solve excersice 09_palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (s) {
+    let processedStrng = s.toLowerCase().replace(/[^a-z0-9]/gi,'');
+    let reversed = processedStrng.split('').reverse().join('');
+    return processedStrng === reversed;
 };
 
 // Do not edit below this line
